cf2793f27900dfcf62f079d71a4721dcd2d298b8
zjianru <864156480@qq.com>
2024-06-18 18:41:41 +0800

    管理端工程搭建，提供接口

---
+++ flash-tp-admin/src/main/java/com/cz/flashtp/admin/trigger/DynamicThreadPoolController.java
@@ -0,0 +1,111 @@
+package com.cz.flashtp.admin.trigger;
+
+
+import com.alibaba.fastjson.JSON;
+import com.cz.flashtp.admin.types.Response;
+import com.cz.flashtp.domain.entity.ThreadPoolConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RList;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * code desc
+ *
+ * @author Zjianru
+ */
+@Slf4j
+@RestController()
+@CrossOrigin("*")
+@RequestMapping("/api/v1/flash/tp/")
+public class DynamicThreadPoolController {
+
+    @Resource
+    public RedissonClient redissonClient;
+
+    /**
+     * 查询线程池数据
+     * curl --request GET \
+     * --url 'http://localhost:8089/api/v1/flash/tp/query_thread_pool_list'
+     */
+    @RequestMapping(value = "query_thread_pool_list", method = RequestMethod.GET)
+    public Response<List<ThreadPoolConfig>> queryThreadPoolList() {
+        try {
+            RList<ThreadPoolConfig> cacheList = redissonClient.getList("THREAD_POOL_CONFIG_LIST_KEY");
+            return Response.<List<ThreadPoolConfig>>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(cacheList.readAll())
+                    .build();
+        } catch (Exception e) {
+            log.error("查询线程池数据异常", e);
+            return Response.<List<ThreadPoolConfig>>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .build();
+        }
+    }
+
+    /**
+     * 查询线程池配置
+     * curl --request GET \
+     * --url 'http://localhost:8089/api/v1/flash/tp/query_thread_pool_config?appName=dynamic-thread-pool-test-app&threadPoolName=threadPoolExecutor'
+     */
+    @RequestMapping(value = "query_thread_pool_config", method = RequestMethod.GET)
+    public Response<ThreadPoolConfig> queryThreadPoolConfig(@RequestParam String appName, @RequestParam String threadPoolName) {
+        try {
+            String cacheKey = "THREAD_POOL_CONFIG_PARAMETER_LIST_KEY" + "_" + appName + "_" + threadPoolName;
+            ThreadPoolConfig ThreadPoolConfig = redissonClient.<ThreadPoolConfig>getBucket(cacheKey).get();
+            return Response.<ThreadPoolConfig>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(ThreadPoolConfig)
+                    .build();
+        } catch (Exception e) {
+            log.error("查询线程池配置异常", e);
+            return Response.<ThreadPoolConfig>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .build();
+        }
+    }
+
+    /**
+     * 修改线程池配置
+     * curl --request POST \
+     * --url http://localhost:8089/api/v1/flash/tp/update_thread_pool_config \
+     * --header 'content-type: application/json' \
+     * --data '{
+     * "appName":"dynamic-thread-pool-test-app",
+     * "threadPoolName": "threadPoolExecutor",
+     * "corePoolSize": 1,
+     * "maximumPoolSize": 10
+     * }'
+     */
+    @RequestMapping(value = "update_thread_pool_config", method = RequestMethod.POST)
+    public Response<Boolean> updateThreadPoolConfig(@RequestBody ThreadPoolConfig request) {
+        try {
+            log.info("修改线程池配置开始 {} {} {}", request.getAppName(), request.getThreadPoolName(), JSON.toJSONString(request));
+            RTopic topic = redissonClient.getTopic("DYNAMIC_THREAD_POOL_REDIS_TOPIC" + "_" + request.getAppName());
+            topic.publish(request);
+            log.info("修改线程池配置完成 {} {}", request.getAppName(), request.getThreadPoolName());
+            return Response.<Boolean>builder()
+                    .code(Response.Code.SUCCESS.getCode())
+                    .info(Response.Code.SUCCESS.getInfo())
+                    .data(true)
+                    .build();
+        } catch (Exception e) {
+            log.error("修改线程池配置异常 {}", JSON.toJSONString(request), e);
+            return Response.<Boolean>builder()
+                    .code(Response.Code.UN_ERROR.getCode())
+                    .info(Response.Code.UN_ERROR.getInfo())
+                    .data(false)
+                    .build();
+        }
+    }
+
+}
--- flash-tp-common/src/main/java/com/cz/flashtp/domain/entity/ThreadPoolConfig.java
+++ flash-tp-common/src/main/java/com/cz/flashtp/domain/entity/ThreadPoolConfig.java
@@ -1,6 +1,5 @@
 package com.cz.flashtp.domain.entity;
 
-import io.netty.handler.traffic.ChannelTrafficShapingHandler;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
